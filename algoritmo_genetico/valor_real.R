a1c321aff3b6d1daa17a4b75a089b1a8a0c1eb8a
Alexander Brandao <69222583+alexander-brandao@users.noreply.github.com>
2022-06-14 17:38:50 -0300

    Add files via upload

---
+++ algoritmo_genetico/valor_real.R
@@ -0,0 +1,28 @@
+#install.packages("GA")
+library(GA)
+
+f <-function(x){
+  
+  #equação 2 * x + 5 = 20
+  resultado = 2 * x + 5
+  
+  if(resultado > 20)
+    return(20 - resultado)
+  else
+    return(resultado - 20)
+}
+
+resposta = ga(
+  "real-value",
+  fitness = f,
+  lower =c(-20),
+  upper=c(20),
+  popSize=10,
+  maxiter =20,
+  monitor = T,
+  names = c("a")
+)
+
+plot(resposta)
+
+
